c2cf88aee5db2e4ca5f8668c65e86c17bbd9d6ed
usman <usman_pervaiz@live.com>
2019-03-30 16:37:26 +0200

    route added for each item

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import App from "../App";
-// import Recipe from "./Recipe";
+import Recipe from "./Recipe";
 
 const Router = () => (
     <BrowserRouter>
         <Switch>
             <Route path="/" component={ App } exact />
-            {/* <Route path="/recipe/:id" component={ Recipe } /> */ }
+            <Route path="/recipe/:id" component={ Recipe } />
         </Switch>
     </BrowserRouter>
 );
