99f90bc5e2f9dd98d35c0996ffbfce076de0a700
usman <usman_pervaiz@live.com>
2019-03-31 15:09:58 +0300

    localstorage for keeping recipes

--- src/App.js
+++ src/App.js
@@ -9,18 +9,26 @@ class App extends Component
   state = {
     recipes: []
   }
-
-
   getRecipe = async ( e ) =>
   {
-    // const recipeName = e.target.elements.recipeName.value;
+    const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    // const api_call = await fetch( `https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${ API_KEY }&q=${ recipeName }&count=10` );
-    const api_call = await fetch( `https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${ API_KEY }&count=10` );
+    const api_call = await fetch( `https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${ API_KEY }&q=${ recipeName }&count=10` );
 
     const data = await api_call.json();
     this.setState( { recipes: data.recipes } );
-    console.log( data );
+    console.log( this.state.recipes );
+  }
+  componentDidMount = () =>
+  {
+    const json = localStorage.getItem( "recipes" );
+    const recipes = JSON.parse( json );
+    this.setState( { recipes } );
+  }
+  componentDidUpdate = () =>
+  {
+    const recipes = JSON.stringify( this.state.recipes );
+    localStorage.setItem( "recipes", recipes );
   }
   render()
   {
@@ -30,7 +38,7 @@ class App extends Component
           <h1 className="App-title">Recipe Search</h1>
         </header>
         <Form getRecipe={ this.getRecipe } />
-        {/* <Recipes recipes={ this.state.recipes } /> */ }
+        <Recipes recipes={ this.state.recipes } />
       </div>
     );
   }
